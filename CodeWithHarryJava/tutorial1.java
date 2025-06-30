2665acca30b4823433e06416099cf9810d137ce2
faizan alam <alamf6023@gmail.com>
2025-06-30 13:10:25 +0530

    no comment

--- CodeWithHarryJava/tutorial1.java
+++ CodeWithHarryJava/tutorial1.java
@@ -1,8 +1,78 @@
 import java.util.Scanner;
 public class tutorial1{
+	public static void main(String args[]){
+		int num = 10;
+		int j = 0;
+
+		while (j <= num){
+			System.out.println("New Value : "+j);
+			j++;
+		}
+
+		do{
+			System.out.println("No Val : "+j+"---"+num);
+			j++;
+		}while(j<num);
+		// For Loop
+		for(int i =0;i<num;i++){
+			System.out.println("Value : "+i);
+		}
+	}
+
+	public static void main7(String args[]){
+		int num1 = 40;
+		int num2 = 70;
+
+		for(int i=0;i<=num1;i++){
+
+			if (i == 30){
+				System.out.println("This is thirty");
+				continue;
+			}
+			
+			if( i % 2 == 0){
+				System.out.println("Even");
+				continue;
+			}
+
+			System.out.println("Value of i :"+i);
+		}
+
+		switch(num1){
+			case 10:
+				System.out.println("Value is 10");
+				break;
+			case 30:
+				System.out.println("Value is 30");
+				break;
+			case 50:
+				System.out.println("Value is 50");
+				break;
+			case 70:
+				System.out.println("Value is 70");
+				break;
+			case 90:
+				System.out.println("Value is 90");
+				break;
+			default:
+				System.out.println("No Value Found!");
+				break;
+
+		}
+		// if(num1 > num2){
+		// 	System.out.println("Num1 is greater than num2");
+		// }
+		// else if(num1 == num2){
+		// 	System.out.println("Num2 is equal to num1");
+		// }
+		// else{
+		// 	System.out.println("Num2 is greater to than num1");
+		// }
+	}
+
 	public static String admin = "Faizan Alam";
 
-	public static void main(String args[]){
+	public static void main6(String args[]){
 		System.out.println("Name of Admin : "+tutorial1.admin);
 		int ag = 14;
 		System.out.println("Age : "+ag);
