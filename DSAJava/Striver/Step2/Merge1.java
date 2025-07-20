99799385a3ac34f5e1e2b07fc1b0d895ba4c9bb0
faizan619 <alamf6023@gmail.com>
2025-07-20 10:54:52 +0530

    striver done

---
+++ DSAJava/Striver/Step2/Merge1.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+public class Merge1{
+	private static void merge(int[] arr, int low, int mid, int high) {
+        ArrayList<Integer> temp = new ArrayList<>(); // temporary array
+        int left = low;      // starting index of left half of arr
+        int right = mid + 1;   // starting index of right half of arr
+
+        //storing elements in the temporary array in a sorted manner//
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.add(arr[left]);
+                left++;
+            } else {
+                temp.add(arr[right]);
+                right++;
+            }
+        }
+
+        // if elements on the left half are still left //
+
+        while (left <= mid) {
+            temp.add(arr[left]);
+            left++;
+        }
+
+        //  if elements on the right half are still left //
+        while (right <= high) {
+            temp.add(arr[right]);
+            right++;
+        }
+
+        // transfering all elements from temporary to arr //
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp.get(i - low);
+        }
+    }
+
+
+
+	public static void mergeSort(int arr[],int low,int high){
+		if(low>=high){	return; }
+		int mid = (low+high)/2;
+		mergeSort(arr,low,mid);
+		mergeSort(arr,mid+1,high);
+		merge(arr,low,mid,high);
+	}
+
+	public static void main(String[] args) {
+		int arr[] = {3,1,4,2,5};
+		int alen = arr.length;
+		mergeSort(arr,0,alen-1);
+		for(int m:arr){
+			System.out.print(m+" ");
+		}
+		System.out.println();
+	}
+}
