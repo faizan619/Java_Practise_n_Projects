cb01b7147302dd8485e11d6a14d112ed27d9e1fb
faizan alam <alamf6023@gmail.com>
2025-07-21 12:02:33 +0530

    starting with controller part and along with that service part

--- Projects/TaskManager/TaskManagerBackend/src/main/java/com/TaskManagerBackend/controller/TaskController.java
+++ Projects/TaskManager/TaskManagerBackend/src/main/java/com/TaskManagerBackend/controller/TaskController.java
@@ -3,11 +3,16 @@ package com.TaskManagerBackend.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.TaskManagerBackend.service.TaskService;
+
 @RestController
 public class TaskController {
+
+    @Autowired
+    private TaskService service;
     
     @GetMapping("/tasks")
     public String getTasks(){
-        return "Getting Tasks";
+        return service.getAllTasks();
     }
 }
---
+++ Projects/TaskManager/TaskManagerBackend/src/main/java/com/TaskManagerBackend/service/TaskService.java
@@ -0,0 +1,12 @@
+package com.TaskManagerBackend.service;
+
+import java.util.List;
+import com.TaskManagerBackend.model.Task;
+
+@Service
+public class TaskService{
+
+    public List<Task> getAllTasks(){
+
+    }
+}
