c85c7305ff827656e8e74f17db177b08c0bf39a1
faizan619 <alamf6023@gmail.com>
2025-07-22 00:21:18 +0530

    minor changes in task project and deleted unused practise projects

--- Projects/TaskManager/TaskManagerBackend/src/main/java/com/TaskManagerBackend/service/TaskService.java
+++ Projects/TaskManager/TaskManagerBackend/src/main/java/com/TaskManagerBackend/service/TaskService.java
@@ -1,12 +1,20 @@
 package com.TaskManagerBackend.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 import com.TaskManagerBackend.model.Task;
+import com.TaskManagerBackend.repository.TaskRepository;
 
 @Service
 public class TaskService{
 
-    public List<Task> getAllTasks(){
+    @Autowired
+    TaskRepository repo;
 
+    public List<Task> getAllTasks(){
+        return repo.findAll();
     }
 }
