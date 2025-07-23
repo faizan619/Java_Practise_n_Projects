d0b1f0bf90af8f83497b7c26e6ae8062c7a4051d
faizan619 <alamf6023@gmail.com>
2025-07-23 23:53:26 +0530

    done with something

--- SpringBoot/javaguide/src/main/java/com/javaguide/JavaguideApplication.java
+++ SpringBoot/javaguide/src/main/java/com/javaguide/JavaguideApplication.java
@@ -2,12 +2,24 @@ package com.javaguide;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class JavaguideApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaguideApplication.class, args);
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(JavaguideApplication.class, args);
+		ComponentClass componentclass = (ComponentClass) applicationContext.getBean(ComponentClass.class);
+		// Both Works
+		// ComponentClass componentclass = (ComponentClass) applicationContext.getBean("componentclass");
+		System.out.println(componentclass.getMessage());
 	}
+}
 
+@Component
+class ComponentClass{
+	public String getMessage(){
+		return "This is the Get Message! From Faizan Alam";
+	}
 }
