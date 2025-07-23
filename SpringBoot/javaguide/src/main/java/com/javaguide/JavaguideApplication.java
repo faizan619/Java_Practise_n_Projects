48ec6d48009be7ca18cd64edc62cfd30a196521d
faizan alam <alamf6023@gmail.com>
2025-07-23 12:21:33 +0530

    java guide learning project

---
+++ SpringBoot/javaguide/src/main/java/com/javaguide/JavaguideApplication.java
@@ -0,0 +1,13 @@
+package com.javaguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaguideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaguideApplication.class, args);
+	}
+
+}
