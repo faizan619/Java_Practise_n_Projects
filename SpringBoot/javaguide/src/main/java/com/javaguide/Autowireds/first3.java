24b23faa3ee6d33fb80feb11a41f9b91c51269df
faizan alam <alamf6023@gmail.com>
2025-07-24 18:18:16 +0530

    done with today

--- SpringBoot/javaguide/src/main/java/com/javaguide/Autowireds/first3.java
+++ SpringBoot/javaguide/src/main/java/com/javaguide/Autowireds/first3.java
@@ -1,14 +1,18 @@
 package com.javaguide.Autowireds;
 
-import org.springframework.context.annotation.Primary;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
 
 @Component("first3")
 // @Primary
 public class first3 implements firstAuto {
+
+    @Value("Faizan Alam")
+    String name;
     
     @Override
     public String getPizza(){
-        return "This is first3 pizza";
+        return "This is first3 "+name;
     }
 }
