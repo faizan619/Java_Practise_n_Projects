e551a65a3ab35513d060e31265f4ec648d2ccf19
faizan alam <alamf6023@gmail.com>
2025-07-29 17:52:58 +0530

    spring react project

--- SpringReactProjects/TodoProject/TodoAppFrontend/src/Components/Tasks.jsx
+++ SpringReactProjects/TodoProject/TodoAppFrontend/src/Components/Tasks.jsx
@@ -1,5 +1,34 @@
+import {useState,useEffect} from "react";
+import axios from "axios"
+
 function Tasks(){
-	return "This is Task Page";
+
+	const [data,setData] = useState([]);
+
+	useEffect(()=>{
+		axios.get("http://localhost:8080/tasks")
+		.then((res)=>{
+			console.log(res.data)
+			setData(res.data)
+		})
+		.catch((err)=>{
+			console.log(err.message)
+		})
+	},[]);
+
+	return <>
+		<div>
+			{
+				data.map((dd,index) => (
+					<div>
+						<li key={index}>{dd.task}</li>
+						<li>{dd.priority? "Important":"not Important"}</li>
+						<br /><br />
+					</div>
+				))
+			}
+		</div>
+	</>;
 }
 
 export default Tasks;
