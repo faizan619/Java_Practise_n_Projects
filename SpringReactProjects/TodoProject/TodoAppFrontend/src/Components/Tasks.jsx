19c62565b663232e8161d996f809a4d8ebf01930
faizan alam <alamf6023@gmail.com>
2025-07-30 17:59:38 +0530

    changes due to pagination

--- SpringReactProjects/TodoProject/TodoAppFrontend/src/Components/Tasks.jsx
+++ SpringReactProjects/TodoProject/TodoAppFrontend/src/Components/Tasks.jsx
@@ -6,12 +6,20 @@ function Tasks(){
 
 	const [data,setData] = useState([]);
 	const navigate = useNavigate();
+	const [currentPage, setCurrentPage] = useState(0);
+	const [totalPages, setTotalPages] = useState(1);  // default to 1 to avoid early disabling
+
 
 	useEffect(()=>{
-		axios.get("http://localhost:8080/tasks")
+		axios.get("http://localhost:8080/tasks",{
+			params:{
+				page:0,
+				size:5
+			}
+		})
 		.then((res)=>{
 			console.log(res.data)
-			setData(res.data)
+			setData(res.data.content)
 		})
 		.catch((err)=>{
 			console.log(err.message)
@@ -24,15 +32,43 @@ function Tasks(){
 		axios.delete(`http://localhost:8080/tasks/${id}`)
 		.then((res)=>{
 			console.log("Task Deleted : ",res);
-			navigate("/tasks");
+			// navigate("/tasks");
+			loadPage(currentPage);  // reload current page
+
 		})
 		.catch((err)=>{
 			console.log("Error :",err);
 		})
 	}
 
+	useEffect(() => {
+		loadPage(0);  // this sets both data and pagination state
+	}, []);
+
+
+	const loadPage = (page) => {
+		axios.get("http://localhost:8080/tasks", {
+			params: {
+				page: page,
+				size: 3
+			}
+		})
+		.then(res => {
+			setData(res.data.content);
+			setCurrentPage(res.data.number);
+			setTotalPages(res.data.totalPages);
+		})
+		.catch(err => {
+			console.error(err.message);
+		});
+	};
+
+
 	return <>
 		<div>
+		<button onClick={() => loadPage(currentPage - 1)} disabled={currentPage === 0}>Previous</button>
+		<button onClick={() => loadPage(currentPage + 1)} disabled={currentPage === totalPages - 1}>Next</button>
+
 			{
 				data.map((dd,index) => (
 					<div className="card mt-2 mx-2 p-2 col-md-4 mx-auto my-4" key={index}>
