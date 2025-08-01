2f0ba8820c7968774a7dc29c57a2f7828d8c6789
faizan alam <alamf6023@gmail.com>
2025-08-01 23:22:03 +0530

    deleted

--- SpringReactProjects/billingApplication/BillingApplicationBackend/src/main/java/com/BillingApplicationBackend/Repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.BillingApplicationBackend.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.BillingApplicationBackend.Model.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Integer>{
-    
-}
