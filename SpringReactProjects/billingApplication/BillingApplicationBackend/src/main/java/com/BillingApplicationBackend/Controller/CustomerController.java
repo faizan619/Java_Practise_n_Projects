9eb3af048988f2a70ef6f5a2a368d41ddcde8bee
faizan alam <alamf6023@gmail.com>
2025-08-01 11:09:57 +0530

    controller part done

--- SpringReactProjects/billingApplication/BillingApplicationBackend/src/main/java/com/BillingApplicationBackend/Controller/CustomerController.java
+++ SpringReactProjects/billingApplication/BillingApplicationBackend/src/main/java/com/BillingApplicationBackend/Controller/CustomerController.java
@@ -6,8 +6,12 @@ import com.BillingApplicationBackend.Model.Customer;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,5 +37,22 @@ public class CustomerController {
     @GetMapping("/customers/{custId}")
     public Customer getCustomerId(@PathVariable int custId){
         return service.getCustomerById(custId);
+    }
+
+    @PostMapping("/customers")
+    public Customer postCustomer(@RequestBody Customer customer){
+
+        return service.saveCustomerDetail(customer);
+    }
+
+    @DeleteMapping("/customers/{custId}")
+    // public String removeCustomerDetails(@PathVariable Integer custId){
+    public String removeCustomerDetails(@PathVariable int custId){
+        return service.deleteCustomer(custId);
+    }
+
+    @PutMapping("/customers/{custId}")
+    public Customer updateCustomerDetails(@PathVariable int custId, @RequestBody Customer updatedDetail){
+        return service.updateDetails(custId, updatedDetail);
     }
 }
