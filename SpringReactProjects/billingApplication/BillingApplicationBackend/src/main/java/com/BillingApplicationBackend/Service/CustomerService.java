37d79dd0067a149d68b86167474ddf5dadd62734
faizan alam <alamf6023@gmail.com>
2025-08-01 11:10:08 +0530

    service part done

--- SpringReactProjects/billingApplication/BillingApplicationBackend/src/main/java/com/BillingApplicationBackend/Service/CustomerService.java
+++ SpringReactProjects/billingApplication/BillingApplicationBackend/src/main/java/com/BillingApplicationBackend/Service/CustomerService.java
@@ -23,4 +23,33 @@ public class CustomerService {
         return repo.findById(custId).orElse(new Customer("Unknown", 0, "unknown@gmail.com", "Unknown", "Unknown", "unknown"));
         // return repo.findById(custId);
     }
+
+    public Customer saveCustomerDetail(Customer customer){
+        return repo.save(customer);
+    }
+
+    // public String deleteCustomer(Integer custId){
+    public String deleteCustomer(int custId){
+        if(repo.existsById(custId)){
+            repo.deleteById(custId);
+            return custId+" is Deleted Successfully!";
+        }
+        else{
+            return "Customer with ID " + custId + " not found.";
+        }
+    }
+
+    public Customer updateDetails(Integer custId, Customer updatedDetails){
+        // return repo.save(details);
+        Customer details = repo.findById(custId).orElseThrow(() -> new RuntimeException("Task with ID " + custId + " not found."));
+
+        details.setContact_person_name(updatedDetails.getContact_person_name());
+        details.setContact(updatedDetails.getContact());
+        details.setEmail(updatedDetails.getEmail());
+        details.setGstin(updatedDetails.getGstin());
+        details.setAddress(updatedDetails.getAddress());
+        details.setCompany_name(updatedDetails.getCompany_name());
+
+        return repo.save(details);
+    }
 }
