65a90f99efed379dc8f7fa033e666ac13ff1e964
faizan619 <alamf6023@gmail.com>
2025-08-06 08:30:46 +0530

    previous project deleted

--- Projects/githubProject/Employee-Management-Sys/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/exception/ExceptionEmployee.java
+++
@@ -1,7 +0,0 @@
-package com.example.EmployeeManagementSystem.exception;
-
-public class ExceptionEmployee extends RuntimeException {
-    public ExceptionEmployee(String message) {
-        super(message);
-    }
-}
