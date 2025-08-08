66c6d693a636e60e08bcadc59f77b28ec974d445
faizan alam <alamf6023@gmail.com>
2025-08-09 00:17:27 +0530

    leanring

---
+++ Practise/finalSpringBoot/JavaLearnings/Java1.java
@@ -0,0 +1,18 @@
+public class Java1 {
+    public static void main(String[] args) {
+        // String num = "56";
+        // int n = (int) num;
+        // System.out.println(n);
+
+        double num = 56.55;
+        int n = (int) num;
+        System.out.println(n);
+
+    }
+    // Variable
+    public static void main1(String[] args) {
+        String name = "Faizan";
+        int age = 22;
+        System.out.println("Name : "+name+" Age : "+age);
+    }
+}
