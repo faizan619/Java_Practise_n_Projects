c72be96ca42ce0cd8e9b77cabb3c98c8b7b5bd0a
faizan alam <alamf6023@gmail.com>
2025-08-11 18:28:01 +0530

    model implemented

--- microservices/monolithic_service/monolithic/src/main/java/com/monolithic/model/Question.java
+++ microservices/monolithic_service/monolithic/src/main/java/com/monolithic/model/Question.java
@@ -1,6 +1,15 @@
 package com.monolithic.model;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Question {
