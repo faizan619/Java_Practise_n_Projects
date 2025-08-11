3b60eb21ea860c15fdae5799eecd6777a0f03e91
faizan alam <alamf6023@gmail.com>
2025-08-11 18:25:34 +0530

    controller impleemnted

--- microservices/monolithic_service/monolithic/src/main/java/com/monolithic/controller/QuestionController.java
+++ microservices/monolithic_service/monolithic/src/main/java/com/monolithic/controller/QuestionController.java
@@ -1,15 +1,27 @@
 package com.monolithic.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.monolithic.model.Question;
+import com.monolithic.service.QuestionService;
+
 @RestController
 @RequestMapping("/question") 
 public class QuestionController {
+
+    @Autowired
+    private QuestionService service;
     
     @GetMapping("/all_questions")
-    public String getAllQuestions(){
-        return "This is the Quetions Page for me    ";
+    public ResponseEntity<List<Question>> getAllQuestions(){
+        List<Question> questions = service.getAllQuestion();
+        return new ResponseEntity<>(questions, HttpStatus.OK);
     }
 }
