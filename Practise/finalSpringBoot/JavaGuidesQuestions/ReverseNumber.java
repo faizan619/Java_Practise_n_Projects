7fa3631aa66fb9e52cdd3763e6d86a72696fdeec
faizan alam <alamf6023@gmail.com>
2025-08-11 10:28:10 +0530

    deleted previous files'

--- Practise/finalSpringBoot/JavaGuidesQuestions/ReverseNumber.java
+++
@@ -1,8 +0,0 @@
-public class ReverseNumber {
-    public static void main(String ag[]) {
-        System.out.println("Reverse Number");
-
-        Integer num = 3453
-        System.out.println(num);
-    }
-}
