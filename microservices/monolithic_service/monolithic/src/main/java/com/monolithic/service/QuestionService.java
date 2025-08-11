9c3e2e651d84ff4ab2497e8f17b39bbafa7e0f98
faizan alam <alamf6023@gmail.com>
2025-08-11 18:25:44 +0530

    service implemented

---
+++ microservices/monolithic_service/monolithic/src/main/java/com/monolithic/service/QuestionService.java
@@ -0,0 +1,20 @@
+package com.monolithic.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.monolithic.model.Question;
+import com.monolithic.repository.QuestionRepository;
+
+@Service
+public class QuestionService {
+    
+    @Autowired
+    private QuestionRepository repo;
+
+    public List<Question> getAllQuestion(){
+        return repo.findAll();
+    }
+}
