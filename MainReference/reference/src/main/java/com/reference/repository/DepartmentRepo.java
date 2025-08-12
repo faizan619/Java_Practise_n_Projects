d11115c2e0ef082c69a8d7c823a3e46346a7d6ce
faizan alam <alamf6023@gmail.com>
2025-08-12 17:07:34 +0530

    repository added

---
+++ MainReference/reference/src/main/java/com/reference/repository/DepartmentRepo.java
@@ -0,0 +1,20 @@
+package com.reference.repository;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.reference.model.Department;
+
+@Repository
+public interface DepartmentRepo extends JpaRepository<Department, Integer> {
+    
+    Department findByDeptName(String deptName);
+    
+    Department findByDeptCode(int deptCode);
+    
+    @Query("SELECT d, u FROM Department d INNER JOIN User u ON d.id = u.deptId")
+    List<Department> findDepartmentsWithUsers();
+    
+}
