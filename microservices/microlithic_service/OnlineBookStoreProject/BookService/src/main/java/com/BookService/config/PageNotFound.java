e31b216033837bc60d8fba36e44db73b77131b28
faizan alam <alamf6023@gmail.com>
2025-08-18 10:55:11 +0530

    page not found

--- microservices/microlithic_service/OnlineBookStoreProject/BookService/src/main/java/com/BookService/config/PageNotFound.java
+++ microservices/microlithic_service/OnlineBookStoreProject/BookService/src/main/java/com/BookService/config/PageNotFound.java
@@ -9,15 +9,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
-public class PageNotFound implements ErrorController{
+public class PageNotFound implements ErrorController {
 
-    @RequestMapping("/page-not-found")
-    public ResponseEntity<Map<String, String>> handleError(){
-        Map<String,String> response = new HashMap<>();
-        response.put("error","Page not Found!");
-        response.put("path","/error");
-        return new ResponseEntity<>(response,HttpStatus.NOT_FOUND);
+    @RequestMapping("/error")
+    public ResponseEntity<Map<String, String>> handleError(HttpServletRequest request) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", "Page not Found!");
+        response.put("path", request.getRequestURI());
+        response.put("status", "404");
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND); // Proper 404
     }
 }
