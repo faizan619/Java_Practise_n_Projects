a2565a10637addbc2ffac7905406ec62a09940d9
faizan alam <alamf6023@gmail.com>
2025-08-20 03:06:30 +0530

    model done

---
+++ microservices/microlithic_service/OnlineBookStoreProject/BookService/src/main/java/com/BookService/model/Books.java
@@ -0,0 +1,38 @@
+package com.BookService.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name="books",schema="practise_book_bookservice")
+public class Books {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int Id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false)
+    private int stock = 0;
+
+}
