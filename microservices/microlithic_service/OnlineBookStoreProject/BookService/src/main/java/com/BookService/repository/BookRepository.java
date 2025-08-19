68460763a8798afc00bed779e5f4c5e04981de30
faizan alam <alamf6023@gmail.com>
2025-08-20 03:06:46 +0530

    repo done'

---
+++ microservices/microlithic_service/OnlineBookStoreProject/BookService/src/main/java/com/BookService/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.BookService.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.BookService.model.Books;
+
+@Repository
+public interface BookRepository extends JpaRepository<Books, Integer>{
+    
+}
