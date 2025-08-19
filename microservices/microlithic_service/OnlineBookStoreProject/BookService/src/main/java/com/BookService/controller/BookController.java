453b6710e64f5fbcd4423526e4afff196e4bcc7f
faizan alam <alamf6023@gmail.com>
2025-08-20 03:07:05 +0530

    config controller done

--- microservices/microlithic_service/OnlineBookStoreProject/BookService/src/main/java/com/BookService/controller/BookController.java
+++ microservices/microlithic_service/OnlineBookStoreProject/BookService/src/main/java/com/BookService/controller/BookController.java
@@ -5,9 +5,19 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
+
+import com.BookService.dto.BookDTO;
+import com.BookService.service.BookService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/books")
@@ -17,7 +27,28 @@ public class BookController {
     private BookService service;
     
     @GetMapping
-    public ResponseEntity<List<Books>> getBooks(){
+    public ResponseEntity<List<BookDTO>> getBooks(){
         return service.getAllBooks();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BookDTO> getBookDetail(@PathVariable int id){
+        return service.getBooksById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<BookDTO> addNewBook(@Valid @RequestBody BookDTO book){
+        return service.addNewBook(book);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<BookDTO> updateBookDetail(@Valid @RequestBody BookDTO book, @PathVariable int id){
+        return service.updateBookDetail(book,id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteByDetail(@PathVariable int id){
+        return service.deleteBookById(id);
+    }
+
 }
