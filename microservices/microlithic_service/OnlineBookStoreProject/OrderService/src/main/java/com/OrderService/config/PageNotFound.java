1a880767e343f250777ab46e695d68d748bb4829
faizan alam <alamf6023@gmail.com>
2025-08-20 21:23:42 +0530

    configuration part done

---
+++ microservices/microlithic_service/OnlineBookStoreProject/OrderService/src/main/java/com/OrderService/config/PageNotFound.java
@@ -0,0 +1,26 @@
+package com.OrderService.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class PageNotFound implements ErrorController {
+
+    @RequestMapping("/error")
+    public ResponseEntity<Map<String, String>> handleError(HttpServletRequest request) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", "Page not Found!");
+        response.put("path", request.getRequestURI());
+        response.put("status", "404");
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND); // Proper 404
+    }
+}
+
