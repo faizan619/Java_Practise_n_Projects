eef6b68bbb805ad743439f1a7b10e9c895701bd3
faizan619 <alamf6023@gmail.com>
2025-08-22 08:21:00 +0530

    array practise started

--- DSA/Arrays/array_practise1.java
+++ DSA/Arrays/array_practise1.java
@@ -12,6 +12,81 @@ public class array_practise1{
 		topics.printAllElementInArray(uarr);
 
 		topics.findMinAndMaxInUnsortedArray(uarr);
+
+		topics.findMinAndMaxInSortedArray(sarr);
+
+		topics.reverseTheUnSortedArray(uarr);
+
+		topics.checkIfTheArrayIsSortedOrNot(uarr);
+
+	}
+
+	public void checkIfTheArrayIsSortedOrNot(int uarr[]) {
+		System.out.println();
+		System.out.println("Checking is the given array is sorted or not");
+		Boolean isArray = false;
+		for(int i=0;i<uarr.length-1;i++) {
+			if(uarr[i] < uarr[i+1]) {
+				isArray = true;
+			}
+			else{
+				isArray = false;
+				break;
+			}
+		}
+
+		for(int u:uarr) {
+			System.out.print(u+" ");
+		}
+		System.out.println();
+		if(isArray) {
+			System.out.println("The Given Array is Sorted Array");
+		}
+		else{
+			System.out.println("The Given array is UnSorted Array");
+		}
+	}
+
+	public void reverseTheUnSortedArray(int uarr[]){
+		System.out.println();
+		System.out.println("Reverse the Unsorted Array");
+		int ulen = uarr.length-1;
+		int urev[] = new int[uarr.length];
+		int inc = 0;
+		System.out.print("Old : ");
+		for(int u:uarr) {
+			System.out.print(u+" ");
+		}
+
+		System.out.println();
+
+		System.out.print("New : ");
+		for(int i=ulen;i>=0;i--) {
+			
+			urev[inc] = uarr[i];
+			inc++;
+		}
+
+		for(int r:urev) {
+			System.out.print(r+" ");
+		}
+		System.out.println();
+	}
+
+	public void findMinAndMaxInSortedArray(int sarr[]){
+		System.out.println();
+		System.out.println("Finding Min and Max Element in the Array");
+		int max = 0;
+		int min = sarr.length-1;
+		for(int u:sarr){
+			if(u>max){
+				max = u;
+			}
+			if(u<min){
+				min = u;
+			}
+		}
+		System.out.println("Maximun value is "+max+" and Minimum value is "+min);
 	}
 
 	public void findMinAndMaxInUnsortedArray(int uarr[]){
@@ -29,7 +104,6 @@ public class array_practise1{
 		}
 		System.out.println("Maximun value is "+max+" and Minimum value is "+min);
 	}
-
 
 	public void printAllElementInArray(int uarr[]){
 		System.out.println();
