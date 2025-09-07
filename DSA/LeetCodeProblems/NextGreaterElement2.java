d82749f6c96b5f1131025df4808c46398540c1ee
faizan619 <alamf6023@gmail.com>
2025-09-07 20:57:00 +0530

    pending

--- DSA/LeetCodeProblems/NextGreaterElement2.java
+++ DSA/LeetCodeProblems/NextGreaterElement2.java
@@ -1,6 +1,21 @@
 public class NextGreaterElement2{
 	public static void main(String[] args) {
 		System.out.println("Next Greater Element 2");
-		
+		int nums[] = {1,2,1};
+
+		int n[] = new int[nums.length];
+
+		int j = 0;
+		for(int i=1;i<nums.length;i++) {
+			if(nums[i] > nums[j] && nums[i] == nums[j]+1) {
+				n[j] = nums[i];
+				j++;
+			}
+			else if(j==nums.length) {
+					n[j] = -1;
+				j++;
+			
+			}
+		}
 	}
 }
